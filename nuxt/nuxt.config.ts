7afa8bf4eb5f3c9ae606942624fbc374d71dd7e4
Philip Hofmann <philiphofmann@hotmail.com>
2025-01-27 16:06:47 +0100

    update

--- nuxt/nuxt.config.ts
+++ nuxt/nuxt.config.ts
@@ -1,6 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  app: {
+    baseURL: '/bhi100-sisr-iqa-metrics/', // baseURL: '/<repository>/'
+    buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
+  },
 
   modules: [
     "@nuxt/ui",
